a0221790d558322e69b7a5273954650b07dd4736
elsehow <yes@cosmopol.is>
2015-11-12 14:35:40 -0800

    added syntax checking - now, when the required file has a syntax error, we remove all the listeners from the emitters and print the syntax error to the stream. no crashing

--- charm.js
+++ charm.js
@@ -5,13 +5,15 @@
  */
 
 var path = require('path')
-  , hotswap = require('hotswap') //overrides `require`
+  , hotswap = require('./hotswap') //overrides `require`
+  , check = require('syntax-error')
+  , fs = require('fs')
   , EventEmitter = require('events').EventEmitter
   , Kefir = require('kefir')
 
 // api:
 //
-//   charm(app, [emitter, event], ...)
+//   charm(absolutePathToFile, [emitter, event], ...)
 //
 module.exports = function () {
 
@@ -21,32 +23,35 @@ module.exports = function () {
     argsList.push(arguments[i])
   }
   // the script user puts in 
-  var app = argsList[0]
+  var appPath = argsList[0]
   // the pairs of [emitter, event]
   var emitEventPairs = argsList.slice(1)
   // if the user was using the single-stream api, turn this arg into a list
   if (!emitEventPairs[0].length)
     emitEventPairs = [emitEventPairs]
 
-  // we run this fn at startup, 
-  // and on reload (when the script is saved)
-  function bootstrap (app) {
-    // remove all our old listeners from the emitters
+  // fn to remove existing listeners from the emitters
+  function removeAllListeners () {
     emitEventPairs.forEach(function (p) {
       p[0].removeAllListeners(p[1])
     })
+  }
+
+  // we run this fn at startup, 
+  // and on reload (when the script is saved)
+  function bootstrap (appFn) {
+
+    // remove all the old listeners
+    removeAllListeners()
+
     // turn the pairs into Kefir streams
     var emitters = emitEventPairs.map(function (p) {
       return Kefir.fromEvents(p[0], p[1])
     })
 
-    // TODO
-    // first, we parse the app script for errors
-    // if we catch one, we print it instead of crashing!
-
     // next, we try/catch executing the function
     try {
-      return app.apply(null, emitters)
+      return appFn.apply(null, emitters)
     } catch (e) {
       // if there's an execution error, we log it instead of crashing!
       console.error('Error!', e)
@@ -59,14 +64,20 @@ module.exports = function () {
   // at the time it was saved
   var emitter = new EventEmitter()
   var returnValStream = Kefir.fromEvents(emitter, 'return-val')
-  var a = require(app)
+  var a = require(appPath)
   function startApp () {
     var v = bootstrap(a)
     emitter.emit('return-val', v)
   }
 
   // we set up the hot-reload functionality here
+  // on every new require, we parse the app script for errors
+  // if we catch one, we print it instead of crashing!
   startApp()
+  hotswap.on('error', function (err) {
+    console.log(err)
+    removeAllListeners() // this will effectively taredown teha pp
+  })
   hotswap.on('swap', function () {
     startApp()
   })
